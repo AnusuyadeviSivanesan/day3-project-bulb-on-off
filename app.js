f978f09f6bd2e7dd12b06cfeac3fea0773b7bbef
AnusuyadeviSivanesan <138973411+AnusuyadeviSivanesan@users.noreply.github.com>
2023-07-12 10:51:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+    img.src="./images/bulb on img.jpeg";
+}
+
+function bulb_off(){
+    img.src="./images/bulb img.jpeg";   
+}
